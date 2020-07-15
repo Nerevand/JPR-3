3d1188d97c04fba229d0825472e5cd40f4854a47
Roman Krychkovskyy <kruchkovskiy2@gmail.com>
2020-07-15 22:48:08 +0300

    add global styles

--- src/dataBase/contentComponent.tsx
+++ src/dataBase/contentComponent.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "./style.css";
-
 export const contentComponent: any = new Map();
 
 contentComponent.set(
--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -4,6 +4,28 @@ const styles = () => ({
       textDecoration: "none",
       color: "inherit",
     },
+
+    ".pre-content-wrapper": {
+      backgroundColor: "rgb(14, 0, 36)",
+      color: "rgb(128, 134, 40)",
+      padding: "15px 20px",
+    },
+
+    ".keyword": {
+      color: "#07a",
+    },
+
+    ".operator": {
+      color: "#a67f59",
+    },
+
+    ".punctuation": {
+      color: "#999",
+    },
+
+    ".number": {
+      color: "#905",
+    },
   },
 });
 
